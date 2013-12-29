2afc9a90e0e31743f156c3cd2dc7eb09bb0a936e
David Hoffmann <david@hoffmann-projcts.de>
2013-12-29 15:35:31 +0100

    Add AppStart class to initialize the app start.
    Signed-off-by: David Hoffmann <david@hoffmann-projcts.de>

---
+++ Core/AppInit.cs
@@ -0,0 +1,36 @@
+//
+//  AppInit.cs
+//
+//  Author:
+//       David Hoffmann <david@hoffmann-projects.de>
+//
+//  Copyright (c) 2013 David Hoffmann
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+using System;
+using System.Threading.Tasks;
+
+namespace de.dhoffmann.xamarin.adfcnewsapp.core
+{
+	public class AppInit
+	{
+		public Task AppStartAsync()
+		{
+			return Task.Factory.StartNew (() => {
+			});
+		}
+	}
+}
+
